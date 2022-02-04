3e47461d0731520afb385cf907b5b151155e5fe4
phozzy <arthur.fayzullin@gmail.com>
2022-02-04 21:04:35 +0100

    Add more options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,7 +1,10 @@
 local options = {
-  signcolumn = "yes",
+  signcolumn = 'yes',
   undofile = true,
-  colorcolumn = "80",
+  colorcolumn = '80',
+  showtabline = 2,
+  showmode = false,
+  cmdheight = 2,
 }
 for k, v in pairs(options) do
   vim.opt[k] = v
