74fac72123bb615a0ce696a9f77f1b360c2b89c5
phozzy <arthur.fayzullin@gmail.com>
2022-02-04 22:19:32 +0100

    Add more options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -5,6 +5,13 @@ local options = {
   showtabline = 2,
   showmode = false,
   cmdheight = 2,
+  completeopt = { "menuone", "noselect" },
+  timeoutlen = 1000,
+  updatetime = 300,
+  writebackup = false,
+  cursorline = true,
+  number = true,
+  list = true,
 }
 for k, v in pairs(options) do
   vim.opt[k] = v
