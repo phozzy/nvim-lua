f194e022fda0d60ece52b24483dcb4f0a3519a25
phozzy <arthur.fayzullin@gmail.com>
2022-02-05 23:54:10 +0100

    Treesitter should not be optional

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -66,6 +66,7 @@ return packer.startup(function(use)
   -- Treesitter
   use {
     "nvim-treesitter/nvim-treesitter",
+    opt = false,
     run = ":TSUpdate",
     config = safe_require_plugin_config("treesitter"),
   }
