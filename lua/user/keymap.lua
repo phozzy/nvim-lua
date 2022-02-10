ed8f9f7081731ca261ad132649c05bb2aefb4474
phozzy <arthur.fayzullin@gmail.com>
2022-02-10 23:39:12 +0100

    Add basic keymappings

--- lua/user/keymap.lua
+++ lua/user/keymap.lua
@@ -19,7 +19,26 @@ vim.g.maplocalleader = " "
 --   command_mode = "c",
 
 -- Normal --
+keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+
 -- Insert --
+-- Press jk fast to enter
+keymap("i", "jk", "<ESC>", opts)
+
 -- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+
 -- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
 -- Terminal --
