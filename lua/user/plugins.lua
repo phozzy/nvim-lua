29a145743d3cbe765d9b66557a629cd8df9f8f81
phozzy <arthur.fayzullin@gmail.com>
2022-02-27 20:08:43 +0100

    Install neogit package

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -168,6 +168,15 @@ return packer.startup(function(use)
   }
   use "nvim-telescope/telescope-file-browser.nvim"
 
+  -- Git
+  use {"TimUntersberger/neogit",
+    opt = false,
+    requires = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = safe_require_plugin_config("neogit"),
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
