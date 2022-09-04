196575bb0f3f5ff58859afb95ee88276b90a9cd9
phozzy <arthur.fayzullin@gmail.com>
2022-09-04 11:53:22 +0200

    Set LSP for installation

--- lua/user/plugin/mason-tool-installer.lua
+++ lua/user/plugin/mason-tool-installer.lua
@@ -3,4 +3,24 @@ if not status_ok then
 	return
 end
 
-mason_tool_installer.setup()
+mason_tool_installer.setup(
+  {
+    ensure_installed = {
+      --LSP
+      "lua-language-server",
+      "luau-lsp",
+      --DAP
+      --Linter
+      "luacheck",
+      "misspell",
+      --Formatter
+      "luaformatter",
+      "stylua",
+    },
+    -- set a delay (in ms) before the installation starts. This is only
+    -- effective if run_on_start is set to true.
+    -- e.g.: 5000 = 5 second delay, 10000 = 10 second delay, etc...
+    -- Default: 0
+    start_delay = 3000, -- 3 second delay
+  }
+)
